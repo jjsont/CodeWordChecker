9ca1f2851d1712165f26fd8869c7f40d0a0c1f3d
BT_1N3_29@BKTECH.LOCAL <BT_1N3_29@BKTECH.LOCAL>
2018-11-19 14:48:17 -0500

    Initial commit

---
+++ src/CodeWordChecker.java
@@ -0,0 +1,23 @@
+public class CodeWordChecker implements StringChecker {
+    private int max;
+    private int min;
+    private String kWord;
+
+public CodeWordChecker(int min, int max, String kWord) {
+    this.min=min;
+    this.max=max;
+    this.kWord= kWord;
+}
+public CodeWordChecker(String kWord){
+    this.min=6;
+    this.max= 20;
+    this.kWord= kWord;
+    }
+public boolean isValid(String str) {
+    boolean isValid= true;
+    if(str.length() <= this.max && str.length() >= this.min && str.indexOf(this.kWord) != -1) {
+        
+    }
+}
+
+}
---
+++ src/StringChecker.java
@@ -0,0 +1,3 @@
+public interface StringChecker {
+    boolean isValue(String str);
+}
